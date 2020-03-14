200111dc425e87806ceb96a5838d656ff70902b1
snlaccount <snlaccount@gmail.com>
2020-03-15 02:09:51 +0530

    first commit

---
+++ src/main/java/com/sample/coronavirustracker/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.sample.coronavirustracker.controller;
+
+import com.sample.coronavirustracker.models.LocationStats;
+import com.sample.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        //model.addAttribute("testName", "TEST");
+        //model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+        return "home";
+    }
+}
---
+++ src/main/java/com/sample/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,64 @@
+package com.sample.coronavirustracker.services;
+
+
+import com.sample.coronavirustracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_DATA_URL_RAW = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    @PostConstruct   //this tells spring to execute this method as soon as it starts, while creating bean
+    @Scheduled(cron = "* * 1 * * *")
+    public void fetchVirsuData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =  HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL_RAW))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //System.out.println(httpResponse.body());
+
+        //parsing csv using apache commons-csv
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records){
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+            locationStats.setLatestTotalCases(latestCases);
+            locationStats.setDiffFromPrevDay(latestCases-prevDayCases);
+
+            newStats.add(locationStats);
+            //String state = record.get("Province/State");
+            //System.out.println(state);
+        }
+        this.allStats = newStats;
+
+    }
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
+}
